d5e1adc4f7b7489a90bf7a5d5def333901ba5966
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2023-12-31 15:37:43 +0530

    Another day1

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,36 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+
+        if(!oldPassword.equals(newPassword)){
+            return;
+        }
+        int totalChars = 0;
+        boolean isOneCharUpper = false;
+        boolean isOneLowerCase = false;
+        boolean containsOneDigit = false;
+        boolean containsSpecial = false;
+        for(int i=0; i<newPassword.length(); i++){
+            char ch = newPassword.charAt(i);
+            if(ch >= 'A' && ch <= 'Z'  || ch >= 'a' && ch <= 'z'){
+                totalChars++;
+            }
+            if(ch >= 'A' && ch <= 'Z'){
+                isOneCharUpper = true;
+            }
+            if(ch >= 'a' && ch <= 'z'){
+                isOneLowerCase = true;
+            }
+            if(ch >= '0' && ch <= '9'){
+                containsOneDigit = true;
+            }
+            if(!Character.isLetterOrDigit(ch) ){
+                containsSpecial = true;
+            }
+        }
+
+        if(totalChars >= 8 && isOneCharUpper && isOneLowerCase && containsOneDigit && containsSpecial){
+            this.password = newPassword;
+        }
     }
 }
