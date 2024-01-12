7866066e59bcd6fbd94c019865e660ab0fed0966
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-01-12 18:51:19 +0530

    Again again v2

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,5 +1,26 @@
 package com.driver.test;
 
+import com.driver.Email;
+import com.driver.Gmail;
+import org.junit.Test;
+
 public class TestCases {
 
+    @Test
+    public void testEmail1() {
+        Email email = new Email("accio@gmail.com");
+        email.changePassword("Accio@123", "Va#1234567");
+        assert (email.getPassword().equals("Va#1234567"));
+    }
+
+    @Test
+    public void testGmail1() {
+        Gmail gmail = new Gmail("accio@gmail.com", 3);
+//        assert (gmail.getEmailId().equals("accio@gmail.com"));
+        gmail.changePassword("Accio@123", "Va#1234567");
+        assert (gmail.getPassword().equals("Va#1234567"));
+//        gmail.changePassword("Va#1234567", "a#1234567");
+//        assert (gmail.getPassword().equals("Va#1234567"));
+//        assert(gmail.getInboxCapacity() == 3);
+    }
 }
